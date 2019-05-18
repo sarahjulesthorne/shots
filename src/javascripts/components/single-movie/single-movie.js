43ee1b6d82bc25f81d918c6ceee2710e67f3a48f
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-18 17:24:40 -0500

    WIP

--- src/javascripts/components/single-movie/single-movie.js
+++ src/javascripts/components/single-movie/single-movie.js
@@ -1,4 +1,7 @@
+import movieData from '../../helpers/data/movies-data';
 import util from '../../helpers/utils';
+let movies = [];
+let singleMovies = [];
 
 const movieBtnListener = () => {
   const movieButtons = Array.from(document.getElementsByClassName('movie-btn'));
@@ -10,6 +13,20 @@ const movieBtnListener = () => {
   });
 };
 
+const filterMoviesArray = (e) => {
+singleMovie = movies.filter(m => e.target.id.includes(m.id));
+};
+
+const initializeSingleMovie = () => {
+movieData.getMoviesData()
+.then((response) => {
+moviesArray = response.data.movies;
+movies = moviesArray;
+})
+};
+
 export default {
   movieBtnListener,
 };
+
+
